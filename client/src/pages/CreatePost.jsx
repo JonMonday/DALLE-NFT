194fa0fbde60233649ad2d5e47c731c371ceb088
JohnMonday <79201337+JonMonday@users.noreply.github.com>
2023-03-03 12:30:57 +0000

    set deployed server

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:3001/api/v1/dallE', {
+        const response = await fetch('https://dalle-nft.onrender.com/api/v1/dallE', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -48,7 +48,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:3001/api/v1/post', {
+        const response = await fetch('https://dalle-nft.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
